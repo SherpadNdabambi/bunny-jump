4f0fe9c0291a41039beb171db112a71180c4ab1f
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-01-04 17:05:57 +0200

    Add background

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -9,9 +9,12 @@ export default class Game extends Phaser.Scene
 
    preload()
    {
+      // load the backgound image
+      this.load.image('background', 'assets/bg_layer1.png')
    }
 
    create()
    {
+      this.add.image(240, 320, 'background')
    }
  }
