848c130f40acad427b904a068f86044bcfd3ca89
Sherpad Ndabambi <sgndabambi@gmail.com>
2023-01-04 18:30:23 +0200

    Add carrots
    1. Add carrots
    2. Add carrot collection

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -1,7 +1,12 @@
 import Phaser from '../lib/phaser.js'
 
+import Carrot from '../game/Carrot.js'
+
 export default class Game extends Phaser.Scene
 {
+   /** @type {Phaser.Physics.Arcade.Group} */
+   carrots
+
    /** @type {Phaser.Types.Input.Keyboard.CursorKeys} */
    cursors
 
@@ -26,6 +31,9 @@ export default class Game extends Phaser.Scene
 
       // load the standing bunny image
       this.load.image('bunny-stand', 'assets/bunny1_stand.png')
+
+      // load the carrot image
+      this.load.image('carrot', 'assets/carrot.png')
 
       // create cursor keys
       this.cursors = this.input.keyboard.createCursorKeys()
@@ -57,7 +65,7 @@ export default class Game extends Phaser.Scene
       // create a bunny sprite
       this.player = this.physics.add.sprite(240, 320, 'bunny-stand').setScale(0.5)
 
-      // add collision
+      // add player-platfrom collision
       this.physics.add.collider(this.platforms, this.player)
 
       // disable other collisions
@@ -70,6 +78,23 @@ export default class Game extends Phaser.Scene
 
       // set the horizontal dead zone to 1.5x game width
       this.cameras.main.setDeadzone(this.scale.width * 1.5)
+
+      // create carrot group
+      this.carrots = this.physics.add.group({
+         classType: Carrot
+      })
+
+      // add carrot-platform collision
+      this.physics.add.collider(this.platforms, this.carrots)
+
+      // add player-carrot overlap
+      this.physics.add.overlap(
+         this.player,
+         this.carrots,
+         this.handleCollectCarrot,  // called on overlap
+         undefined,
+         this
+      )
    }
 
    update(t, dt)
@@ -83,6 +108,9 @@ export default class Game extends Phaser.Scene
          {
             platform.y = scrollY - Phaser.Math.Between(50, 100)
             platform.body.updateFromGameObject()
+
+            // create a carrot above the platform being reused
+            this.addCarrotAbove(platform)
          }
       })
 
@@ -113,23 +141,54 @@ export default class Game extends Phaser.Scene
 
       // wrap player around
       this.horizontalWrap(this.player)
+   }
 
-      // add player wrap-around logic
-      /**
-       * @param {Phaser.GameObjects.Sprite} sprite
-       */
-      horizontalWrap(sprite)
+   // add player wrap-around logic
+   /**
+    * @param {Phaser.GameObjects.Sprite} sprite
+    */
+   horizontalWrap(sprite)
+   {
+      const halfWidth = sprite.displayWidth * 0.5
+      const gameWidth = this.scale.width
+      if (sprite.x < -halfWidth)
       {
-         const halfWidth = sprite.displayWidth * 0.5
-         const gameWidth = this.scale.width
-         if (sprite.x < -halfWidth)
-         {
-            sprite.x = gameWidth + halfWidth
-         }
-         else if (sprite.x > gameWidth + halfWidth)
-         {
-            sprite.x = -halfWidth   
-         }
+         sprite.x = gameWidth + halfWidth
       }
+      else if (sprite.x > gameWidth + halfWidth)
+      {
+         sprite.x = -halfWidth   
+      }
+   }
+
+   /**
+    * @param {Phaser.GameObjects.Sprite} sprite
+    */
+   addCarrotAbove(sprite)
+   {
+      const y = sprite.y - sprite.displayHeight
+
+      /** @type {Phaser.Physics.Arcade.Sprite} */
+      const carrot = this.carrots.get(sprite.x, y, 'carrot')
+
+      this.add.existing(carrot)
+
+      // update the physics body size
+      carrot.body.setSize(carrot.width, carrot.height)
+
+      return carrot
+   }
+
+   /**
+    * @param {Phaser.Physics.Arcade.Sprite} player
+    * @param {Carrot} carrot
+    */
+   handleCollectCarrot(player, carrot)
+   {
+      // hide from display
+      this.carrots.killAndHide(carrot)
+
+      // disable from physics world
+      this.physics.world.disableBody(carrot.body)
    }
  }
